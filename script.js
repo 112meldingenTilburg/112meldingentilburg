45fbbc28eb90f2e703c97f0351ee9260d370e021
112meldingenTilburg <51676751+112meldingenTilburg@users.noreply.github.com>
2019-06-10 21:44:45 +0200

    0.1

---
+++ script.js
@@ -0,0 +1,24 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+});
+} )( jQuery );
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
